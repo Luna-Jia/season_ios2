bb7584d101dbabeffc84af8b0c8649b9b6cb9a40
Luna Meng Jia <luna.jia6@gmail.com>
2024-04-22 18:50:17 -0400

    Japanese Food

--- CL08-Dynamic-Nav/CL08-Dynamic-Nav/MySubViewController.swift
+++ CL08-Dynamic-Nav/CL08-Dynamic-Nav/MySubViewController.swift
@@ -9,17 +9,36 @@ import UIKit
 
 class MySubViewController: UIViewController {
     
-    var cv_img : String = "Spring.jpg"
+    var cv_img : String = "Miso Soup.jpg"
+    var cv_recipeText: String = ""
+    var cv_recipeName: String = ""
+    var cv_title: String = ""  // Add this property to store the title for the navigation item
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        //self.navigationItem.title = cv_model
-               //vv_labelModel.text = cv_model
-               vv_imageView.image = UIImage(named: cv_img)
+        self.navigationItem.title = cv_title  // Set the navigation bar title
+        vv_imageView.image = UIImage(named: cv_img)
+        recipe.text = cv_recipeText
+        
+        // Set the background color based on the recipe name
+                view.backgroundColor = backgroundColor(for: cv_recipeName)
     }
     
+    // Determine background color based on the recipe name
+        private func backgroundColor(for recipeName: String) -> UIColor {
+            switch recipeName {
+            case "Miso Soup":
+                return UIColor(red: 0.67, green: 0.84, blue: 1.00, alpha: 1.00)
+            case "Mochi":
+                return UIColor(red: 0.96, green: 1.00, blue: 0.98, alpha: 1.00)  // Mint color
+            case "Salmon Teriyaki":
+                return UIColor(red: 1.00, green: 0.92, blue: 0.23, alpha: 1.00)  // Dark yellow
+            default:
+                return UIColor.white
+            }
+        }
+    
+    @IBOutlet weak var recipe: UILabel!
     @IBOutlet weak var vv_imageView: UIImageView!
     
 }
--- CL08-Dynamic-Nav/CL08-Dynamic-Nav/ViewController.swift
+++ CL08-Dynamic-Nav/CL08-Dynamic-Nav/ViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var cv_arrayLabel: [String] = ["Spring","Summer","Fall","Winter"]
-    var cv_arrayImg: [String] = ["t1.png", "t2.png", "t3.png", "t4.png"]
-    var cv_arrayImg2: [String] = ["Spring.jpg", "Summer.jpg", "Fall.jpg", "Winter.jpg"]
+    var cv_arrayLabel: [String] = ["Miso Soup","Mochi","Salmon Teriyaki"]
+    var cv_arrayImg: [String] = ["Miso Soup.png", "Mochi.png", "Salmon Teriyaki.png"]
+    var cv_arrayImg2: [String] = ["Miso Soup.jpg", "Mochi.jpg", "Salmon Teriyaki.jpg"]
+    var cv_arrayRecipeLabel: [String] = ["In a medium saucepan over medium-high heat, combine dashi granules and water; bring to a boil. Reduce heat to medium, and whisk in the miso paste. Stir in tofu. Separate the layers of the green onions, and add them to the soup. Simmer gently for 2 to 3 minutes before serving.","Wrap red bean paste in aluminum foil and place in the freezer for at least 3 hours. Mix sweet rice flour and green tea powder thoroughly in a microwave-safe glass or ceramic bowl. Stir in water, then sugar. Mix until smooth. Cover bowl with plastic wrap.Cook the rice flour mixture in the microwave for 3 minutes and 30 seconds. Meanwhile, remove red bean paste from the freezer and divide paste into 8 equal balls. Set aside. Stir rice flour mixture and heat for another 15 to 30 seconds.","Mix sesame oil, lemon juice, soy sauce, brown sugar, sesame seeds, ground mustard, ginger, and garlic powder in a small saucepan over low heat. Bring to a simmer, stirring until sugar has dissolved. Set aside 1/2 cup of marinade for basting.Pour remaining marinade into a resealable plastic bag and place salmon into the marinade. Squeeze air out of the bag, seal, and marinate the salmon steaks for at least 1 hour (2 hours for better flavor). Drain and discard used marinade."]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,12 +41,17 @@ class ViewController: UITableViewController {
         }
     
     @IBOutlet var vv_tableView: UITableView!
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "vv_segueForward") {
             if let destination = segue.destination as? MySubViewController {
                 if let lv_index = vv_tableView.indexPathForSelectedRow {
+                    let selectedRecipeName = cv_arrayLabel[lv_index.row]  // Get the recipe name
                     //destination.cv_model = cv_arrayModel[lv_index.row]
                     destination.cv_img = cv_arrayImg2[(lv_index as NSIndexPath).row]
+                    destination.cv_recipeText = cv_arrayRecipeLabel[lv_index.row]
+                    destination.cv_recipeName = selectedRecipeName  // Pass the recipe name
+                    destination.cv_title = selectedRecipeName 
                 }
             }
         }
